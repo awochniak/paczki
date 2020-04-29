9f6c1fc7ac6ce111dd29d985db42371ef75b16d5
Arkadiusz Wochniak <arkadiusz.wochniak@przelewy24.pl>
2020-04-29 13:59:31 +0200

    test

--- Package.swift
+++ Package.swift
@@ -2,3 +2,12 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
+let package = Package(
+    name: "allegro",
+    products: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio", from: "1.12.0")
+    ],
+    targets: []
+)
