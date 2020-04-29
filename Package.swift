f238a41f38cf50934519f6b2b7955dabbe9dfde2
Arkadiusz Wochniak <arkadiusz.wochniak@przelewy24.pl>
2020-04-29 13:26:24 +0200

    test

--- Package.swift
+++ Package.swift
@@ -5,18 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "allegro",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "core", targets: ["p24-core"]),
-        .library(name: "paymentMethods", targets: ["p24-payment-methods"])
-    ],
+    products: [],
     dependencies: [],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-//        .target(
-//            name: "ttt",
-//            dependencies: []),
-
-    ]
+    targets: []
 )
